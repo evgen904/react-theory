b8cdd3d7fe7148927350d1d0f933386977ddde08
Evgen904 <evgen904@gmail.com>
2020-03-28 23:29:47 +0300

    Ограничения

--- src/App.js
+++ src/App.js
@@ -3,27 +3,32 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  // return (
-  //   <div className="App">
-  //     <h1>Hello World!</h1>
-  //   </div>
-  // );
+  return (
+    // в JSX всегда должен быть корневой элемент
+    // className это атрибут class
+    <div>
+      <div className="App">
+        <h1>Hello World!</h1>
+      </div>
+      <p>Hello</p>
+    </div>    
+  );
 
   // интерпритатор реакт запись выше, посути реализует так:
-  return React.createElement(
-    'div',
-    {
-      className: 'App'
-    },
-    React.createElement(
-      // название тега
-      'h1',
-      // пропсы 
-      null,
-      // содержимое текста
-      'Hello world !'
-    )
-  )
+  // return React.createElement(
+  //   'div',
+  //   {
+  //     className: 'App'
+  //   },
+  //   React.createElement(
+  //     // название тега
+  //     'h1',
+  //     // пропсы 
+  //     null,
+  //     // содержимое текста
+  //     'Hello world !'
+  //   )
+  // )
 }
 
 export default App;
