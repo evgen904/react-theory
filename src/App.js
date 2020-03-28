c60a98d7b123a0245012ab5488f3c4bff35f21cc
Evgen904 <evgen904@gmail.com>
2020-03-28 23:21:19 +0300

    Как работает JSX

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+// визде где используется JSX нужно импортировать React, т.к. собирается через createElement
 import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
+  // return (
+  //   <div className="App">
+  //     <h1>Hello World!</h1>
+  //   </div>
+  // );
+
+  // интерпритатор реакт запись выше, посути реализует так:
+  return React.createElement(
+    'div',
+    {
+      className: 'App'
+    },
+    React.createElement(
+      // название тега
+      'h1',
+      // пропсы 
+      null,
+      // содержимое текста
+      'Hello world !'
+    )
+  )
 }
 
 export default App;
