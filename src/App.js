3e352d6addbfa61e15591bf333023791cea5489f
Evgen904 <evgen904@gmail.com>
2020-03-29 22:20:07 +0300

    Создание State

--- src/App.js
+++ src/App.js
@@ -1,45 +1,38 @@
-// визде где используется JSX нужно импортировать React, т.к. собирается через createElement
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Car from './Car/Car';
 
-function App() {
-  const divStyle = {
-    textAlign: 'center'
-  }
-  return (
-    // в JSX всегда должен быть корневой элемент
-    // className это атрибут class
-    <div style={divStyle}>
-      <div className="App">
-        <h1 style={{color: 'green', fontSize: '22px'}}>Hello World!</h1>
-      </div>
-      <p>Hello</p>
-      <Car name={'Ford'} year={2018}>
-        <p style={{color: 'blue'}}>Color</p>
-      </Car>
-      <Car name='Mazda' year={2010}>
-        <p style={{color: 'red'}}>Color</p>
-      </Car>
-      <Car name='Audi' year={2019} />
-    </div>    
-  );
+class App extends Component {
 
-  // интерпритатор реакт запись выше, посути реализует так:
-  // return React.createElement(
-  //   'div',
-  //   {
-  //     className: 'App'
-  //   },
-  //   React.createElement(
-  //     // название тега
-  //     'h1',
-  //     // пропсы 
-  //     null,
-  //     // содержимое текста
-  //     'Hello world !'
-  //   )
-  // )
+  state = {
+    cars: [
+      {name: 'Ford', year: 2010},
+      {name: 'Mazda', year: 2018},
+      {name: 'Audi', year: 2020}
+    ],
+    pageTitle: 'React components'
+  }
+
+  render() {
+    const divStyle = {
+      textAlign: 'center'
+    }
+
+    const cars = this.state.cars;
+
+    return (
+      <div style={divStyle}>
+        <div className="App">
+          <h1 style={{color: 'green', fontSize: '22px'}}>
+           {this.state.pageTitle}
+          </h1>
+          <Car name={cars[0].name} year={cars[0].year} />
+          <Car name={cars[1].name} year={cars[1].year} />
+          <Car name={cars[2].name} year={cars[2].year} />
+        </div>        
+      </div>    
+    );
+  }  
 }
 
 export default App;
