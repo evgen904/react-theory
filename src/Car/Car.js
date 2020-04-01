a23bc2ae8e8f12a3a6c4a39ec928d3494894ed8b
Evgen904 <evgen904@gmail.com>
2020-04-02 01:05:58 +0300

    Подключение CSS

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,14 +1,8 @@
 import React from 'react';
+import './Car.css';
 
 export default props => (
-  <div style={{
-    display: 'inline-block',
-    border: '1px solid #ccc',
-    margin: '0 10px 10px 0',
-    paddng: '10px',
-    boxShadow: '0 4px 5px 0 rgba(0,0,0, .14)',
-    borderRadius: '5px'
-  }}>
+  <div className="Car">
     <h3>Car name: {props.name}</h3>
     <p>Year: <strong>{props.year}</strong></p>
     <input type="text" onChange={props.onChangeName} value={props.name} />
